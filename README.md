f89e97ddb55c7cacfb193ebadb540ea9607979f7
priamai <paolo@priam.ai>
2024-07-23 23:42:57 +0100

    Initial POC.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Stix Talker
 Conversational query language for your stix needs!
+
+```
+SQL Prompt: [{'role': 'system', 'content': "You are a SQLite expert. Please help to generate a SQL query to answer the question. Your response should ONLY be based on the given context and follow the response guidelines and format instructions. ===Response Guidelines \n1. If the provided context is sufficient, please generate a valid SQL query without any explanations for the question. \n2. If the provided context is almost sufficient but requires knowledge of a specific string in a particular column, please generate an intermediate SQL query to find the distinct strings in that column. Prepend the query with a comment saying intermediate_sql \n3. If the provided context is insufficient, please explain why it can't be generated. \n4. Please use the most relevant table(s). \n5. If the question has been asked and answered before, please repeat the answer exactly as it was given before. \n"}, {'role': 'user', 'content': 'How many indicators do we have?'}, {'role': 'assistant', 'content': 'SELECT COUNT(*) FROM indicator'}, {'role': 'user', 'content': 'How many indicators are in total?'}, {'role': 'assistant', 'content': 'SELECT COUNT(*) FROM indicator'}, {'role': 'user', 'content': 'How many indicators do we have?'}]
+Using model gpt-4o for 251.0 tokens (approx)
+
+
+Ran 2 tests in 8.085s
+
+OK
+LLM Response: SELECT COUNT(*) FROM indicator
+   COUNT(*)
+0         1
+```
+
--- tests/sqlite_test.py
+++ tests/sqlite_test.py
@@ -1,13 +1,19 @@
+import os
 import unittest
-
+import ujson
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
+from firepit import BundleManager
 
 from vanna.openai import OpenAI_Chat
 from vanna.chromadb import ChromaDB_VectorStore
 from helpers import tmp_storage,tmp_sql_storage
+import stix2
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
 
 class MyVanna(ChromaDB_VectorStore, OpenAI_Chat):
     def __init__(self, config=None):
@@ -16,9 +22,51 @@ class MyVanna(ChromaDB_VectorStore, OpenAI_Chat):
 
 class SqliteStix(unittest.TestCase):
     def test_loader(self):
-        vn = MyVanna(config={'api_key': 'sk-...', 'model': 'gpt-4-...'})
-        tmp_sql_storage("stix.sqlite","stix")
+
+        store = tmp_sql_storage("stix.sqlite","stix")
+
+        with open("./test_bundle.json", 'r') as fp:
+            data = ujson.loads(fp.read())
+
+            bundle_in = stix2.parse(data, allow_custom=True)
+            BundleManager.write_bundle(store, bundle_in)
+
+            # list all bundles
+            bundle_ids = BundleManager.read_bundle_ids(store)
+
+            assert len(bundle_ids) == 1
+
+            store.delete()
+
+    def test_vanna(self):
+        vn = MyVanna(config={'api_key': os.getenv("OPEN_AI"), 'model': 'gpt-4o'})
+
+        from stix2 import Indicator
+        from stix2 import Bundle
+
+        indicator = Indicator(name="File hash for malware variant",
+                              pattern="[file:hashes.md5 = 'd41d8cd98f00b204e9800998ecf8427e']",
+                              pattern_type="stix")
+
+        store = tmp_sql_storage("stix.sqlite", "stix",clear=True)
+
+        bundle = Bundle(indicator)
+
+        BundleManager.write_bundle(store, bundle)
+
         vn.connect_to_sqlite("stix.sqlite")
+
+        vn.train(
+            question="How many indicators are in total?",
+            sql="SELECT COUNT(*) FROM indicator"
+        )
+
+        sql = vn.generate_sql("How many indicators do we have?")
+
+        result = vn.run_sql(sql)
+
+        print(result)
+        store.delete()
 
 
 if __name__ == '__main__':
